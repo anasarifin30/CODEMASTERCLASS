2c704f8eff97c9d5c854436a978d97605b1e2662
anasarifin30 <anasarifin30@students.amikom.ac.id>
2023-11-23 16:31:14 +0700

    Materi Kelas

---
+++ app/src/main/java/com/example/codemasterclass/MateriUIActivity.kt
@@ -0,0 +1,11 @@
+package com.example.codemasterclass
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MateriUIActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_materi_uiactivity)
+    }
+}
